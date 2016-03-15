c75c0a68abb7708ce63a160b4c6a2748dec0287a
Thuong Ho <thuong.t.ho@gmail.com>
2016-03-15 01:29:32 -0700

    main layout

---
+++ pint-scraper/public/js/controllers.js
@@ -0,0 +1,47 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('MainCtrl', MainCtrl);
+
+    MainCtrl.$inject = ['$scope', '$http'];
+
+    function MainCtrl($scope, $http){
+      $scope.item = {};
+      $scope.scrapePostForm = true;
+      $scope.showScrapeDetails = false;
+      $scope.loading = false;
+      $scope.gotScrapeResults = false;
+      $scope.alertContainer = false;
+
+      // Watch for changes to URL, Scrape & Display the image
+      $scope.$watch("item.link", function(newVal, oldVal){
+        // console.log('newVal: ', newVal, 'oldVal: ', oldVal);
+        if (newVal.length > 5) {
+          $scope.loading = true;
+          $http.post('/api/scrape', {
+            url: $scope.item.link
+          })
+          .then(function(data) {
+            console.log(data);
+            $scope.showScrapeDetails = true;
+            $scope.gotScrapeResults = true;
+            $scope.item.imgThumb = data.data.img;
+            $scope.item.description = data.data.desc;
+          }, function(error){
+            console.log('failed to return from scrape');
+            $scope.loading = false;
+            $scope.item.link = "";
+            $scope.gotScrapeResults = false;
+          })
+          .finally(function(){
+            $scope.loading = false;
+          });
+        }
+      });
+    }
+    // .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+      
+    // }])
+})();
--- pint-scraper/routes.js
+++ pint-scraper/routes.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+// include model
+var Item = require('./model/Item.model');
 
 router.get('/test', function(req, res){
   res.send('hello from api/test');
