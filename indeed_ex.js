6a0a42049c275640b365b31747b817122075ff50
Thuong Ho <thuong.t.ho@gmail.com>
2016-03-14 23:56:40 -0700

    add routes

--- indeed_ex.js
+++ indeed_ex.js
@@ -24,9 +24,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // route
 app.get('/', function(req, res){
+  var url = 'https://jobs.lever.co/shopco/bf2b584e-8c74-4b7d-848f-831f8fa7c70f?lever-source=indeed';
+  request(url, function(err, resp, body){
+    var $ = cheerio.load(body);
+    var companyName = $('.posting-headline h2');
+    var companyNameText = companyName.text();
+
+    var jobTitle = $('.posting-headline h2');
+    var jobTitleText = jobTitle.text();
+
+    var location = $('.medium-category-label');
+    var locationText = location.text();
+
+    var summary = $('.section div:first-child').first();
+    var summaryText = summary.text();
+
+    var details = {
+      companyName: companyNameText,
+      jobTitle: jobTitleText,
+      location: locationText,
+      summary: summaryText,
+      url: url
+    };
+
+    // need to change obj to array for some reason
+    // lodash change to array
+    var detailsArray = _(details).toArray();
+
+    console.log(detailsArray);
+    return res.render('index', {
+      jobInfo: detailsArray
+    });
+  });
+  
+
   // express will look into views folder for file index
   // views b/c we set views as directory
-  res.render('index');
+  // res.render('index');
 });
 
 // start server
--- pint-scraper/server.js
+++ pint-scraper/server.js
@@ -2,18 +2,27 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+// reference external route
+var routes = require('./routes');
 
 var port = 8100;
 mongoose.connect('mongodb://localhost/pintScraper');
 
 var app = express();
 
+// setting views path as the views folder
 app.set('views', path.join(__dirname, 'views'));
+// view engine to html using ejs
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+// body-parser to use and parse json obj
 app.use(bodyParser.json());
+// in order to use public folder and all client side objects in that folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// use routes at the end before launching server
+app.use('/api', routes);
 
 app.listen(port, function(){
   console.log('running server on port ' + port);
